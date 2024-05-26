beafcbc14bef37593f09100b8309c10ab2733bd0
irfankhanlashari <165201699+irfankhanlashari@users.noreply.github.com>
2024-05-26 19:55:11 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer =await inquirer.prompt([
+    {message:"type your first number", type:"number",name:"firstNumber"},
+    {message:"type your second number", type:"number",name:"secondNumber"},
+    {message:"select your operator", type:"list",name:"operator", 
+    choices:["addition","multiply","divide","substrection"],},
+])
+if(answer.operator==="addition"){
+    console.log(`your answer is ${answer.firstNumber + answer.secondNumber}`);
+}
+else if(answer.operator==="multiply"){
+    console.log(`your answer is ${answer.firstNumber * answer.secondNumber}`);
+}
+else if(answer.operator==="divide"){
+    console.log(`your answer is ${answer.firstNumber / answer.secondNumber}`);
+}
+else if(answer.operator==="substrection"){
+    console.log(`your answer is ${answer.firstNumber - answer.secondNumber}`);
+}
+else {
+    console.log("please enter valid number");
+    
+}
